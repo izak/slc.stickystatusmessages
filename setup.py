69b2b072335a88b8b3099bd6a5ab5ae803cee037
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-04-15 14:27:02 +0000

    Add z3c.autoinclude.plugin, and other goodies
    git-svn-id: https://svn.plone.org/svn/collective/slc.stickystatusmessages/trunk@115381 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -3,28 +3,33 @@ import os
 
 version = '1.0'
 
-setup(name='slc.stickystatusmessages',
-      version=version,
-      description="Provides sticky status messages to members for all the folders in which they are assigned local roles",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='slc.stickystatusmessages',
+    version=version,
+    description="Provides sticky status messages to members for all the folders in which they are assigned local roles",
+    long_description=open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
         ],
-      keywords='statusmessage status sticky',
-      author='JC Brand, Syslab.com GmbH',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['slc'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    keywords='statusmessage status sticky',
+    author='JC Brand, Syslab.com GmbH',
+    author_email='brand@syslab.com',
+    url='http://plone.org/products/slc.stickystatusmessages',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir = {'' : 'src'},
+    namespace_packages=['slc'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+    """,
+    setup_requires=["PasteScript"],
+    paster_plugins = ["ZopeSkel"],
+    )
