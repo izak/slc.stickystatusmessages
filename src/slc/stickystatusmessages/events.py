277f045aea7e6448a2f55fee3310f249f45ee530
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-05-21 10:52:16 +0000

    removed log statemens and fixed a bug when folder.Title() is None
    git-svn-id: https://svn.plone.org/svn/collective/slc.stickystatusmessages/trunk@117932 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/slc/stickystatusmessages/events.py
+++ src/slc/stickystatusmessages/events.py
@@ -7,15 +7,15 @@ log = logging.getLogger('slc.stickystatusmessages/events.py')
 
 def object_copied_event(obj, evt):
     """ """
-    log.info('object_copied_event')
     folder = obj.aq_parent
+    folder_title = folder.Title() or ''
     message = _(
                 u'%s <a href="%s">%s</a> has been copied into <a href="%s">%s</a>' \
                                                 % ( obj.portal_type, 
                                                     '/'.join(obj.getPhysicalPath()), 
                                                     obj.Title().decode('utf-8'), 
                                                     '/'.join(folder.getPhysicalPath()),
-                                                    folder.Title().decode('utf-8'), 
+                                                    folder_title.decode('utf-8'), 
                                                     )
                 )
     utils.set_sticky_status_message(obj, message)
@@ -29,14 +29,14 @@ def object_moved_event(obj, evt):
     if 'portal_factory' in evt.oldParent.getPhysicalPath():
         return
 
-    log.info('object_moved_event')
     folder = evt.newParent
+    folder_title = folder.Title() or ''
     message = _(
                 u'%s <em>%s</em> moved into <a href="%s">%s</a>' \
                                         % ( obj.portal_type, 
                                             obj.Title().decode('utf-8'), 
                                             '/'.join(folder.getPhysicalPath()),
-                                            folder.Title().decode('utf-8'), 
+                                            folder_title.decode('utf-8'), 
                                             )
                 )
     utils.set_sticky_status_message(obj, message)
@@ -44,14 +44,14 @@ def object_moved_event(obj, evt):
 
 def object_removed_event(obj, evt):
     """ """
-    log.info('object_removed_event')
     folder = obj.aq_parent
+    folder_title = folder.Title() or ''
     message = _(
                 u'%s <em>%s</em> removed from <a href="%s">%s</a>' \
                                         % ( obj.portal_type, 
                                             obj.Title().decode('utf-8'), 
                                             '/'.join(folder.getPhysicalPath()),
-                                            folder.Title().decode('utf-8'), 
+                                            folder_title.decode('utf-8'), 
                                             )
                 )
     utils.set_sticky_status_message(obj, message)
@@ -59,15 +59,15 @@ def object_removed_event(obj, evt):
 
 def object_created_event(obj, evt):
     """ """
-    log.info('object_created_event')
     folder = obj.aq_parent
+    folder_title = folder.Title() or ''
     message = _(    
                 u'%s <a href="%s">%s</a> created in <a href="%s">%s</a>' \
                                     % ( obj.portal_type, 
                                         '/'.join(obj.getPhysicalPath()), 
                                         obj.Title().decode('utf-8'), 
                                         '/'.join(folder.getPhysicalPath()),
-                                        folder.Title().decode('utf-8'), 
+                                        folder_title.decode('utf-8'), 
                                         )
                 )
     utils.set_sticky_status_message(obj, message)
@@ -75,15 +75,15 @@ def object_created_event(obj, evt):
 
 def object_edited_event(obj, evt):
     """ """
-    log.info('object_edited_event')
     folder = obj.aq_parent
+    folder_title = folder.Title() or ''
     message = _(
                 u'%s <a href="%s">%s</a> edited in <a href="%s">%s</a>' \
                                     % ( obj.portal_type, 
                                         '/'.join(obj.getPhysicalPath()), 
                                         obj.Title().decode('utf-8'), 
                                         '/'.join(folder.getPhysicalPath()),
-                                        folder.Title().decode('utf-8'), 
+                                        folder_title.decode('utf-8'), 
                                         )
                 )
     utils.set_sticky_status_message(obj, message)
@@ -91,8 +91,8 @@ def object_edited_event(obj, evt):
 
 def object_state_changed_event(obj, evt):
     """ """
-    log.info('object_state_changed_event')
     folder = obj.aq_parent
+    folder_title = folder.Title() or ''
     workflowTool = getToolByName(obj, "portal_workflow")
     chain = workflowTool.getChainFor(obj)
     status = workflowTool.getStatusOf(chain[0], obj)
@@ -104,7 +104,7 @@ def object_state_changed_event(obj, evt):
                         '/'.join(obj.getPhysicalPath()), 
                         obj.Title().decode('utf-8'), 
                         '/'.join(folder.getPhysicalPath()),
-                        folder.Title().decode('utf-8'), 
+                        folder_title.decode('utf-8'), 
                         state, 
                         )
                 )
